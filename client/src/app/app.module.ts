b5abd8555714c6cac03680fb5f3d330514446735
samih1999 <97765397+samih1999@users.noreply.github.com>
2022-08-08 11:18:14 +0300

    end 6

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,6 +9,12 @@ import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { LitsComponent } from './lits/lits.component';
+import { MessagesComponent } from './messages/messages.component';
+import {ToastrModule} from 'ngx-toastr';
+import { SharedModule } from './_modules/shared.module';
 
 
 
@@ -17,8 +23,13 @@ import { RegisterComponent } from './register/register.component';
     AppComponent,
     NavComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    MemberDetailComponent,
+    LitsComponent,
+    MessagesComponent
   ],
+  
   imports: [
    
     BrowserModule,
@@ -26,7 +37,8 @@ import { RegisterComponent } from './register/register.component';
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    BsDropdownModule.forRoot()
+   SharedModule
+ 
     
  ],
   providers: [],
