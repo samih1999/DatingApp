72ca3dc0012a67d361035193ab287138585564bf
samih1999 <97765397+samih1999@users.noreply.github.com>
2022-08-10 15:20:49 +0300

    ssssss

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,17 +1,17 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import{HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
-import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
-import { LitsComponent } from './lits/lits.component';
 import { MessagesComponent } from './messages/messages.component';
 import {ToastrModule} from 'ngx-toastr';
 import { SharedModule } from './_modules/shared.module';
@@ -22,8 +22,9 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
-
-
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { LoadingInterceptor } from './_interceptors/loading.interceptor';
+import { LitsComponent } from './lits/lits.component';
 
 @NgModule({
   declarations: [
@@ -41,23 +42,19 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
     MemberCardComponent,
     MemberEditComponent
   ],
-  
   imports: [
-   
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-   SharedModule
- 
-    
- ],
-  providers: [{
-    provide:HTTP_INTERCEPTORS,useClass:ErrorInterceptor,multi:true},
-    {provide:HTTP_INTERCEPTORS,useClass:JwtInterceptor,multi:true}
-
-  
+    SharedModule,
+    NgxSpinnerModule
+  ],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true}
   ],
   bootstrap: [AppComponent]
 })
