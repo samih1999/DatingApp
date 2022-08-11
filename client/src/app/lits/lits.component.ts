91ec6478fe7a8e0b5ef8d69c8acc4e10afab9da5
samih1999 <97765397+samih1999@users.noreply.github.com>
2022-08-11 15:45:33 +0300

    sami 123

---
+++ client/src/app/lits/lits.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lits',
+  templateUrl: './lits.component.html',
+  styleUrls: ['./lits.component.css']
+})
+export class LitsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
